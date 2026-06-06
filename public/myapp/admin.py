90e9e5eb391df193365c2f070c49c560f7118a58
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-06-06 11:47:56 +0530

    Add public/myapp/admin.py

---
+++ public/myapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Login)
+admin.site.register(Category)
+admin.site.register(Skill)
+admin.site.register(Organizer)
+admin.site.register(Volunteer)
+admin.site.register(Event)
+admin.site.register(Task)
+admin.site.register(Application)
+admin.site.register(TaskAssignment)
+admin.site.register(Attendance)
+admin.site.register(PerformanceReview)
+admin.site.register(Notification)
